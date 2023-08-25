fbd5727d29317342c88a7a8ecdd9175b110a3bec
Fernando Cristiani <fernando.cristiani@joyn.de>
2023-08-25 12:49:50 +0200

    Add basic home view with template engine

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import express, { Router } from 'express';
+import mustacheExpress from 'mustache-express';
 import { API_PORT } from './config';
 
 const app = express();
+
+app.engine('mustache', mustacheExpress());
+app.set('view engine', 'mustache');
+app.set('views', __dirname + '/views');
 
 const api = () => {
   const apiRouter = Router();
@@ -12,7 +17,18 @@ const api = () => {
   return apiRouter;
 };
 
+const views = () => {
+  const viewsRouter = Router();
+
+  viewsRouter.get('/', (_, res) => {
+    res.render('home');
+  });
+
+  return viewsRouter;
+};
+
 app.use('/api', api());
+app.use('/', views());
 
 app.listen(API_PORT, () => {
   console.log(`[server]: Server is running at port ${API_PORT}`);
